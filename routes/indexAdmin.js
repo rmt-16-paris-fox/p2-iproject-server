9f128627f288927021c9e0f5152131a29dac0ebf
Samuel Matthew <samuelmatthew211@gmail.com>
2021-10-16 23:33:11 +0700

    :sparkles: Show all keyboard done

--- routes/indexAdmin.js
+++ routes/indexAdmin.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const KeyboardController = require('../controllers/KeyboardController');
 const authentication = require('../middlewares/authentication');
 const authorization = require('../middlewares/authorization');
 const router = express.Router();
 
 router.use(authentication);
 router.use(authorization);
-router.get('/keyboards', (req, res) => {
-	res.send('hello');
-});
+router.get('/keyboards', KeyboardController.showAllKeyboards);
 
 module.exports = router;
