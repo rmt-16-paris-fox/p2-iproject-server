104e0cb343ed5b9bf9b83b18aecf8577847f3c67
Samuel Matthew <samuelmatthew211@gmail.com>
2021-10-16 22:07:05 +0700

    :building_construction: Move register, login, and login google for customer from PublicController to UserController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,102 @@
 const { User } = require('../models');
 
-class UserController {}
+class UserController {
+	static async registerCustomer(req, res, next) {
+		try {
+			const { email, password, fullName } = req.body;
+
+			const response = await User.create({
+				email,
+				password,
+				fullName,
+				role: 'Customer',
+			});
+
+			res.status(201).json({ id: response.id, email: response.email });
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	static async loginCustomer(req, res, next) {
+		try {
+			const { email, password } = req.body;
+
+			const response = await User.findOne({
+				where: { email: email || null },
+			});
+
+			if (!response) {
+				throw { name: 'unauthorized' };
+			}
+
+			if (!comparePassword(password, response.password)) {
+				throw { name: 'unauthorized' };
+			}
+
+			const payload = {
+				id: response.id,
+				email: response.email,
+			};
+
+			const access_token = createToken(payload);
+
+			res.status(200).json({ access_token });
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	// static async loginGoogle(req, res, next) {
+	// 	try {
+	// 		const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+	// 		const { google_token } = req.body;
+
+	// 		const ticket = await client.verifyIdToken({
+	// 			idToken: google_token,
+	// 			audience: process.env.GOOGLE_CLIENT_ID,
+	// 		});
+
+	// 		const payload = ticket.getPayload();
+
+	// 		const emailFromGoogle = payload.email;
+	// 		const nameFromGoogle = payload.name;
+
+	// 		let role = 'Customer';
+	// 		if (emailFromGoogle === 'samuelmatthew211@gmail.com') {
+	// 			role = 'Administrator';
+	// 		}
+
+	// 		const [user, created] = await User.findOrCreate({
+	// 			where: { email: emailFromGoogle },
+	// 			defaults: {
+	// 				email: emailFromGoogle,
+	// 				password: Math.random().toString(36).slice(-10),
+	// 				role,
+	// 				fullName: nameFromGoogle,
+	// 			},
+	// 		});
+
+	// 		const tokenPayload = {
+	// 			id: user.id,
+	// 			email: user.email,
+	// 		};
+
+	// 		const access_token = createToken(tokenPayload);
+
+	// 		if (created === true) {
+	// 			res.status(201).json({
+	// 				id: user.id,
+	// 				email: user.email,
+	// 				access_token: access_token,
+	// 			});
+	// 		} else {
+	// 			res.status(200).json({ access_token });
+	// 		}
+	// 	} catch (err) {
+	// 		next(err);
+	// 	}
+	// }
+}
 
 module.exports = UserController;
