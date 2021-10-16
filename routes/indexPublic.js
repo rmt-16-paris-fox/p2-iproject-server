15145487bb4720546cecbf896b4e61c3f28d4153
Samuel Matthew <samuelmatthew211@gmail.com>
2021-10-16 19:46:20 +0700

    :sparkles: Create GET /public/keyboards. :card_file_box: Add more attributes in table 'Images'

--- routes/indexPublic.js
+++ routes/indexPublic.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 router.post('/register', PublicController.register);
 router.post('/login', PublicController.login);
-router.post('/login-google', PublicController.loginGoogle);
+// router.post('/login-google', PublicController.loginGoogle);
+
+router.get('/keyboards', PublicController.showKeyboardGallery);
 
 router.use(authentication);
-router.post('/keyboard', PublicController.createKeyboard);
+router.post('/keyboards', PublicController.orderKeyboard);
 
 module.exports = router;
