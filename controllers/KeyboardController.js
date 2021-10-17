77a7af1d36c8eafd8fb50a9d0ba9bea6022fd1c2
Samuel Matthew <samuelmatthew211@gmail.com>
2021-10-17 14:45:31 +0700

    :truck: Move addImage from KeyboardController to ImageController

--- controllers/KeyboardController.js
+++ controllers/KeyboardController.js
@@ -132,35 +132,6 @@ class KeyboardController {
 			next(err);
 		}
 	}
-
-	static async addImages(req, res, next) {
-		try {
-			const KeyboardId = Number(req.params.keyboardId);
-			const { imageUrls } = req.body;
-
-			if (!KeyboardId) {
-				throw { name: 'invalid req.params' };
-			}
-
-			const targetKeyboard = await Keyboard.findOne({
-				where: { id: KeyboardId },
-			});
-
-			if (!targetKeyboard) {
-				throw { name: 'keyboard not found' };
-			}
-
-			const data = imageUrls.map((el) => {
-				return { imageUrl: el, KeyboardId };
-			});
-
-			const response = await Image.bulkCreate(data);
-
-			res.status(201).json(response);
-		} catch (err) {
-			next(err);
-		}
-	}
 }
 
 module.exports = KeyboardController;
