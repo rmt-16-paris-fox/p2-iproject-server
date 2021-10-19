1718dffc553684baf95eabb0e369b3bd9d70db7f
Your Name <ikmal.amrin45@gmail.com>
2021-10-19 01:53:46 -0700

    baru handle login register, login

---
+++ helpers/jwt.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+const secret = process.env.RAHASIA;
+
+const createToken = (payload) => {
+  return jwt.sign(payload, secret);
+};
+const verifyToken = (token) => {
+  return jwt.verify(token, secret);
+};
+
+module.exports = {
+  createToken,
+  verifyToken,
+};
---
+++ models/user.js
@@ -0,0 +1,57 @@
+'use strict';
+const {hash} = require('../helpers/bcrypt')
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    username: {
+      allowNull:false,
+      type:DataTypes.STRING,
+      validate:{
+        notEmpty:true
+      }
+    },
+    email: {
+      allowNull:false,
+      unique:true,
+      type:DataTypes.STRING,
+      validate:{
+        notEmpty:true
+      }
+    },
+    password: {
+      allowNull:false,
+      type:DataTypes.STRING,
+      validate:{
+        notEmpty:true
+      }
+    },
+    address: {
+      allowNull:false,
+      type:DataTypes.STRING,
+      validate:{
+        notEmpty:true
+      }
+    }
+  }, {
+    hooks:{
+      beforeCreate: (user)=>{
+        user.password = hash(user.password)
+      }
+    },
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
