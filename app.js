f5c42d45f529f999308c2a20ec6f2a7d081178fc
giovannyptr <giovannyputri@ymail.com>
2021-10-20 03:01:37 +0700

    setup app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require('./routes/index')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use(routes)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
