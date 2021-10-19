64251b63f7621bd5233fb05689acf05818fc036b
Samuel Matthew <samuelmatthew211@gmail.com>
2021-10-19 12:00:44 +0700

    :sparkles: Send request to xendit done

--- helpers/xendit.js
+++ helpers/xendit.js
@@ -1,3 +1,47 @@
+const axios = require('axios');
+const { Keyboard } = require('../models');
+
+const ovoCharge = async (req, res, next) => {
+	try {
+		const phoneNumber = req.body.phoneNumber;
+		const keyboardId = Number(req.body.keyboardId);
+		const time = new Date();
+
+		const payload = {
+			reference_id: `${req.user.email}-${keyboardId}-${time}`,
+			currency: 'IDR',
+			amount: 250000,
+			checkout_method: 'ONE_TIME_PAYMENT',
+			channel_code: 'ID_OVO',
+			channel_properties: {
+				mobile_number: phoneNumber,
+			},
+		};
+
+		const axiosInstance = axios.create({
+			baseURL: 'https://api.xendit.co/ewallets/charges',
+		});
+
+		const response = await axiosInstance({
+			method: 'POST',
+			url: '/',
+			data: payload,
+			auth: { username: process.env.XENDIT_API_KEY },
+		});
+
+		await Keyboard.update(
+			{ ChargeId: response.data.id },
+			{
+				where: { id: keyboardId },
+			}
+		);
+
+		res.status(200).json(response.data);
+	} catch (err) {
+		next(err.response);
+	}
+};
+
 const ovoPay = async (req, res, next) => {
 	try {
 		res.send(req.body);
@@ -14,4 +58,4 @@ const ovoStatus = async (req, res, next) => {
 	}
 };
 
-module.exports = { ovoPay, ovoStatus };
+module.exports = { ovoCharge, ovoPay, ovoStatus };
--- routes/indexPublic.js
+++ routes/indexPublic.js
@@ -2,12 +2,12 @@ const express = require('express');
 const KeyboardController = require('../controllers/KeyboardController');
 const UserController = require('../controllers/UserController');
 const fetchYoutubeVideos = require('../helpers/fetchYouTube');
-const { ovoPay, ovoStatus } = require('../helpers/xendit');
+const { ovoPay, ovoStatus, ovoCharge } = require('../helpers/xendit');
 const authentication = require('../middlewares/authentication');
 const router = express.Router();
 
-router.post('/ovo', ovoPay);
-router.post('/ovo/status', ovoStatus);
+// router.post('/ovo/status', authentication, ovoStatus);
+// router.post('/ovo', authentication, ovoPay);
 
 router.post('/register', UserController.registerCustomer);
 router.post('/login', UserController.loginCustomer);
@@ -18,6 +18,8 @@ router.get('/keyboards/:id', KeyboardController.showKeyboardDetails);
 router.get('/videos', fetchYoutubeVideos);
 
 router.post('/keyboards', authentication, KeyboardController.orderKeyboard);
+router.post('/ovo/charge', authentication, ovoCharge);
+
 router.get('/my-keyboards', authentication, KeyboardController.showMyKeyboard);
 
 module.exports = router;
