d7879d85bcc06f038cd366b59621544273135873
mrslnputra <mrslnputra@gmail.com>
2021-10-20 02:02:17 +0700

    finish create route for get comment by post including its controller

--- router/comment.js
+++ router/comment.js
@@ -2,5 +2,5 @@ const CommentController = require('../controllers/commentController')
 const commentRoute = require('express').Router()
 
 commentRoute.post('/:id', CommentController.createComment)
-
+commentRoute.get('/:id', CommentController.getCommentByPost)
 module.exports = commentRoute
