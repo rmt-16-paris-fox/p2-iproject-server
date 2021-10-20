753325ef277e7a53d0c51497dc277102422e1650
mrslnputra <mrslnputra@gmail.com>
2021-10-20 22:30:36 +0700

    add route for get user data including its controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,6 +52,18 @@ class UserController {
       next(err)
     }
   }
+
+  static async getUserData(req,res,next){
+    try {
+      const id = +req.user.id
+      const user = await User.findByPk(id, {
+        attributes: ['id', 'email']
+      })
+      res.status(200).json(user)
+    } catch (err) {
+      next(err)
+    }
+  }
 }
 
 module.exports = UserController
