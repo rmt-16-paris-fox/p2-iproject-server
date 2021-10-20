6f3ef2f23166a35e722df894fea944d3bd91a431
mrslnputra <mrslnputra@gmail.com>
2021-10-21 06:00:25 +0700

    add socket io to server

---
+++ router/comment.js
@@ -0,0 +1,8 @@
+const CommentController = require('../controllers/commentController')
+const commentRoute = require('express').Router()
+
+commentRoute.post('/:id', CommentController.createComment)
+commentRoute.get('/:id', CommentController.getCommentByPost)
+commentRoute.delete('/:commentId', CommentController.deleteCommentById)
+
+module.exports = commentRoute
