2fd5b9c2b7c52a957246ddc65069a4add63c446e
Darwin <darwinsantoso@yahoo.com>
2021-10-20 23:00:25 +0700

    📝 added API documentation

--- models/review.js
+++ models/review.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Review.belongsTo(models.User, { foreignKey: 'userId' });
-      Review.belongsTo(models.Book, { as: 'reviews', foreignKey: 'bookId' });
+      Review.belongsTo(models.Book, { foreignKey: 'bookId' });
     }
   }
   Review.init(
