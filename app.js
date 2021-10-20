ea39c36d471db08af3b8e8980785b897c645a864
dandyraspudi <dandyraspudi@gmail.com>
2021-10-21 05:55:14 +0700

    deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routers');
 const cors = require('cors');
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors());
 app.use(express.json());
