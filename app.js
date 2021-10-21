626f75c271af6f1c687615ce595032ca8820b1b6
damarvijis <damar.j@mail.ugm.ac.id>
2021-10-21 11:30:42 +0700

    heroku

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 const cors = require("cors");
 const authentication = require('./middleware/authentication')
 const authorization = require('./middleware/authorization')
