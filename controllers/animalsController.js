5ab0f908d6d9baa4fffcfc2b1561c5cefb4f5bb7
Savicta <vicaci@protonmail.com>
2021-10-21 10:55:43 +0800

    fixed animalsRouter

--- controllers/animalsController.js
+++ controllers/animalsController.js
@@ -10,14 +10,17 @@ class Controller {
       next(err);
     }
   }
-  static async fetchAnimals(req, res, next) {
+  static async fetchData(req, res, next) {
     try {
       const result = await Animal.findAll({
+        order: [
+          ['updatedAt', 'DESC']
+        ],
         attributes: {
           exclude: ['createdAt', 'updatedAt']
         }
       });
-      res.status(200).json(result);
+      res.status(200).json({ result: result, name: req.user.name });
     } catch (err) {
       next(err);
     }
@@ -59,10 +62,33 @@ class Controller {
             id: req.params.animalId
           }
         });
-        res.status(200).json({ message: 'Animal with id ' + req.params.animalId + ' has been deleted' });
+        if (foundAnimal) {
+          res.status(200).json({ message: 'Animal with id ' + req.params.animalId + ' has been deleted' });
+        } else {
+          res.status(404).json({ message: 'Animal not found' });
+        }
       }
     } catch (err) {
       next(err);
+    }
+  }
+  static async findAnimal(req, res, next) {
+    try {
+      const foundAnimal = await Animal.findOne({
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        },
+        where: {
+          name: req.params.animalName
+        }
+      });
+      if (foundAnimal) {
+        res.status(200).json(foundAnimal);
+      } else {
+        res.status(404).json({ message: 'Animal not found' });
+      }
+    } catch (err) {
+
     }
   }
 }
--- routes/animalsRouter.js
+++ routes/animalsRouter.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const Controller = require('../controllers/animalsController');
 
 router.post('/addAnimal', Controller.addAnimal);
-router.get('/fetchAnimals', Controller.fetchAnimals);
+router.get('/fetchData', Controller.fetchData);
 router.put('/updateAnimal/:animalId', Controller.updateAnimal);
 router.delete('/deleteAnimal/:animalId', Controller.deleteAnimal);
+router.get('/findAnimal/:animalName', Controller.findAnimal);
 
 module.exports = router;
