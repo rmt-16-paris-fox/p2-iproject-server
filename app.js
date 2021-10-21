efe84fdcdd8f9d76cdee177bae44a9db6eddaa18
ColinMaximillion <ryuken_allstar@yahoo.com>
2021-10-21 11:04:55 +0700

    log not done

--- app.js
+++ app.js
@@ -17,16 +17,49 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(router);
 
+const { User } = require("./models");
+
 let users = [];
 
 io.on("connection", (socket) => {
   console.log("user connected", socket.id);
-  socket.on("disconnect", () => {
+
+  socket.on("log", async (msg) => {
     // socket.emit("get-rooms", rooms);
+    try {
+      // console.log(msg);
+      const foundItem = await User.findOne({
+        where: {
+          name: msg.name,
+        },
+      });
+      if (!foundItem) {
+        // Item not found, create a new one
+        const item = await User.create({
+          name: msg.name,
+          chatLog: JSON.stringify(msg.log),
+        });
+        return { item, created: true };
+      }
+      // Found an item, update it
+      const item = await User.update(
+        { name: msg.name, chatLog: JSON.stringify(msg.log) },
+        {
+          where: {
+            name: msg.name,
+          },
+        }
+      );
+      // console.log({ item, created: false });
+      return { item, created: false };
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   socket.on("newMessage", (msg) => {
     // console.log(msg);
+
     io.emit("sendMessage", {
       message: msg,
       user: socket.username,
@@ -39,9 +72,19 @@ io.on("connection", (socket) => {
     console.log("User connected - Username: " + socket.username);
   });
 
-  socket.on("loginUser", (user) => {
+  socket.on("loginUser", async (user) => {
     users.push(user);
     console.log(users);
+    try {
+      const foundItem = await User.findOne({
+        where: {
+          name: user,
+        },
+      });
+      io.emit("logMessage", JSON.parse(foundItem.dataValues.chatLog));
+    } catch (err) {
+      console.log(err);
+    }
   });
 });
 
