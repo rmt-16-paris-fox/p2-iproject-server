fd8c37a28df8ff7c03b0207f89860e6479ec22db
ColinMaximillion <ryuken_allstar@yahoo.com>
2021-10-22 11:34:34 +0700

    update home

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const Controller = require("../controller/controller.js");
 const errorHandler = require("../middlewares/errorHandler.js");
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+  res.send("this is home");
+});
 router.get("/estate", Controller.getAll);
 router.get("/city", Controller.getCity);
 router.get("/chat", Controller.chat);
